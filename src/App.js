2f6e42f4afa0bb099315662e2adc6c7ef2d8bc08
Renan Guerra <53278938+renanguerra@users.noreply.github.com>
2020-03-22 11:07:44 -0300

    Commit Inicial

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Login from "./views/login/login"
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World!</h1>
+      <Login/>
+    </div>
+  );
+}
+
+export default App;
